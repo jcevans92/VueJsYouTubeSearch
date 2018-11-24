3f90535a0acfec9d9521cbf4a08b58e5ac74579a
Jeremy Evans <chiefgeekstudios@outlook.com>
2018-11-24 11:38:03 -0500

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList></VideoList>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
+const API_KEY = 'AIzaSyAOw2-eg17-NkFyOiwBKy4QBUx8RVrIfKM';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    name: 'App',
+    components: {
+        SearchBar,
+        VideoList
+    },
+    data: function() { // Because component. This needs to be function
+        return {
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+        }
+    },
+    methods: {
+        onTermChange(searchTerm) {
+            
+            axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params:{
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            }).then(response => console.log(response));
+        }
+    }
+};
+</script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <!--<input v-on:input="onTextInput" />-->
+        <input @input="onTextInput" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    methods: {
+        onTextInput(event) {
+            this.$emit('termChange', event.target.value);
+        }
+    }
+};
+</script>
+
+<style>
+</style>
