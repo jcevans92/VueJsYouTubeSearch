1d6310205f70e8924540560df4c6df746b467e01
Jeremy Evans <chiefgeekstudios@outlook.com>
2018-11-24 12:28:02 -0500

    Create v-for for VideoItemList.

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,13 +1,23 @@
 <template>
     <ul>
-        VideoList
-        {{ videos.length }}
+        <VideoListItem 
+            v-for="video in videos" 
+            :video="video"
+            :key="video.etag"
+            >
+
+        </VideoListItem>
     </ul>
 </template>
     
 <script>
+import VideoListItem from './VideoListItem';
+
 export default {
     name: 'VideoList',
+    components: {
+        VideoListItem
+    },
     props: {
         videos: Array
     } // Properties that will come from parent component, Use object for type checking, Recommended!
